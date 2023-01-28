5f71943ac4a33d8adc63a689630d2c4c8158a716
hsh8220 <hsh8220@gmail.com>
2023-01-28 23:27:31 +0900

    first commit

---
+++ src/main/java/com/erp/hangilse/HangilseApplication.java
@@ -0,0 +1,13 @@
+package com.erp.hangilse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangilseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangilseApplication.class, args);
+    }
+
+}
